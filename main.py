6b5af38f4ac34d6ee5c3e3ca9b862d6cbefa9c02
Ramiro Rocha <therealrockyrocha@gmail.com>
2018-10-31 20:26:54 -0500

    Updated main.py
    Fixed an error in main.py and changed some formatting issues.

--- main.py
+++ main.py
@@ -119,16 +119,12 @@ while True:
 
             # send the player back the list of possible commands
             mud.send_message(id, "Commands:")
-            mud.send_message(id, "  say <message>  - Says something out loud, "
-                                 + "e.g. 'say Hello'")
-            mud.send_message(id, "  look           - Examines the "
-                                 + "surroundings, e.g. 'look'")
-            mud.send_message(id, "  go <exit>      - Moves through the exit "
-                                 + "specified, e.g. 'go outside'")
-
+            mud.send_message(id, "  say <message>  - Says something out loud, e.g. 'say Hello'")
+            mud.send_message(id, "  look           - Examines the surroundings, e.g. 'look'")
+            mud.send_message(id, "  go <exit>      - Moves through the exit specified, e.g. 'go outside'")
+            mud.send_message(id, "  money [unit]   - Shows your balance. if no unit is specified, it lists all of them is short form.")
         # 'say' command
         elif command == "say":
-
             # go through every player in the game
             for pid, pl in players.items():
                 # if they're in the same room as the player
@@ -136,10 +132,8 @@ while True:
                     # send them a message telling them what the player said
                     mud.send_message(pid, "{} says: {}".format(
                                                 players[id]["name"], params))
-
         # 'look' command
         elif command == "look":
-
             # store the player's current room
             rm = rooms[players[id]["room"]]
 
@@ -163,7 +157,6 @@ while True:
             # send player a message containing the list of exits from this room
             mud.send_message(id, "Exits are: {}".format(
                                                     ", ".join(rm["exits"])))
-
         # 'go' command
         elif command == "go":
 
@@ -183,7 +176,6 @@ while True:
                         # send them a message telling them that the player
                         # left the room
                         mud.send_message(pid, "{} left via exit '{}'".format(players[id]["name"], ex))
-
                 # update the player's current room to the one the exit leads to
                 players[id]["room"] = rm["exits"][ex]
                 rm = rooms[players[id]["room"]]
@@ -197,7 +189,6 @@ while True:
                         # send them a message telling them that the player
                         # entered the room
                         mud.send_message(pid, "{} arrived via exit '{}'".format( players[id]["name"], ex))
-
                 # send the player a message telling them where they are now
                 mud.send_message(id, "You arrive at '{}'".format(players[id]["room"]))
 
@@ -205,14 +196,13 @@ while True:
             else:
                 # send back an 'unknown exit' message
                 mud.send_message(id, "Unknown exit '{}'".format(ex))
-
         # some other, unrecognised command
         elif command == "money":
             # give a amount of a certain type
-            if params == "gold" or params == "silver" or params == "copper":
-                mud.send_message(id, "You have {} {} pieces".format(players[id]["money"][params]), params)
+            if params in players[id]["money"].keys():
+                mud.send_message(id, "You have {} {} pieces".format(players[id]["money"][params], params))
             else:
-                mud.send_message(id, "You have {} gp, {} sp, and {} cp".format(players[id]["money"]["gold"], players[id]["money"]["silver"], players[id]["money"]["copper"])
-        else:
+                mud.send_message(id, "You have {} gp, {} sp, and {} cp".format(players[id]["money"]["gold"], players[id]["money"]["silver"], players[id]["money"]["copper"]))
+        else :
             # send back an 'unknown command' message
             mud.send_message(id, "Unknown command '{}'".format(command))
